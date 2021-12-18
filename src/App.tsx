4c023bf2365af8592aee3e93d6d6848ad261236c
Vinícius Moreira <viniciusmoreeira@icloud.com>
2021-12-18 14:31:48 -0300

    fix: tplus stage handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider } from 'native-base';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider, focusManager } from 'react-query';
+
+import useAppState from '@hooks/useAppState';
 
 import Routes from './routes';
 import theme from './styles/theme';
@@ -17,6 +19,12 @@ const config = {
 const queryClient = new QueryClient();
 
 export default function App() {
+  const { justBecameActive } = useAppState();
+
+  React.useEffect(() => {
+    focusManager.setFocused(justBecameActive);
+  }, [justBecameActive]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <NativeBaseProvider theme={theme} config={config}>
