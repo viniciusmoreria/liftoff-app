641cf7157c9b51f7978f62236dc1108a27a8ce10
Vinícius Moreira <viniciusmoreeira@icloud.com>
2021-12-19 00:25:55 -0300

    feat: implements custom greeting and new counter styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import BottomSheet, {
+  BottomSheetTextInput,
+  BottomSheetBackdrop,
+} from '@gorhom/bottom-sheet';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import LottieView from 'lottie-react-native';
 import { Box, Center, Heading, ScrollView, StatusBar, Text } from 'native-base';
-import { RefreshControl, TouchableOpacity } from 'react-native';
+import { Keyboard, RefreshControl, TouchableOpacity } from 'react-native';
 import { useQueryClient } from 'react-query';
 
 import { LoadingAnimation } from '@assets/animations';
@@ -17,6 +22,7 @@ import theme from '../../styles/theme';
 
 export default function Home() {
   const queryClient = useQueryClient();
+  const bottomSheetRef = React.useRef<BottomSheet>(null);
 
   const { isLoading: isLoadingLaunches, isError: errorUpcomingLaunches } =
     useUpcomingLaunches();
@@ -24,13 +30,54 @@ export default function Home() {
   const { isLoading: isLoadingPastLaunches, isError: errorPastLaunches } =
     usePastLaunches();
 
+  const snapPoints = React.useMemo(() => [360], []);
+
   const [refreshing, setRefreshing] = React.useState(false);
+  const [name, setName] = React.useState('');
+  const [storagedName, setStoragedName] = React.useState('');
 
   const handleRefreshData = React.useCallback(async () => {
     setRefreshing(true);
     await queryClient.refetchQueries();
     setRefreshing(false);
   }, [queryClient]);
+
+  const handleSubmitName = React.useCallback(async () => {
+    Keyboard.dismiss();
+    setStoragedName(name.trim());
+    setName('');
+    await AsyncStorage.setItem('name', name.trim());
+
+    setTimeout(() => {
+      bottomSheetRef.current?.close();
+    }, 100);
+  }, [name]);
+
+  const renderBackdrop = React.useCallback(
+    (props) => (
+      <BottomSheetBackdrop
+        {...props}
+        pressBehavior="close"
+        opacity={0.8}
+        disappearsOnIndex={-1}
+        appearsOnIndex={0}
+        style={{ ...props.style, backgroundColor: theme.colors.secondary }}
+      />
+    ),
+    [],
+  );
+
+  React.useLayoutEffect(() => {
+    async function getName() {
+      const username = await AsyncStorage.getItem('name');
+
+      if (username?.length) {
+        setStoragedName(username);
+      }
+    }
+
+    getName();
+  }, []);
 
   if (isLoadingLaunches || isLoadingPastLaunches) {
     return <LoadingComponent />;
@@ -41,41 +88,109 @@ export default function Home() {
   }
 
   return (
-    <ScrollView
-      flex={1}
-      bg="background"
-      pr="4"
-      refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={handleRefreshData}
-          tintColor="white"
-          colors={['white']}
-        />
-      }
-    >
-      <StatusBar animated barStyle="light-content" />
+    <>
+      <ScrollView
+        flex={1}
+        bg="background"
+        pr="4"
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={handleRefreshData}
+            tintColor="white"
+            colors={['white']}
+          />
+        }
+      >
+        <StatusBar animated barStyle="light-content" />
 
-      <AnimatedBox>
-        <Box safeArea mt="6">
-          <Heading color="white" fontWeight="500" pl="4">
-            {greeting()}
-          </Heading>
-        </Box>
-      </AnimatedBox>
+        <AnimatedBox>
+          <Box safeArea mt="6" pl="4">
+            <Heading color="white" fontWeight="500">
+              {greeting()},
+            </Heading>
 
-      <AnimatedBox delay={500}>
+            <Box alignSelf="flex-start">
+              <TouchableOpacity
+                onPress={() => bottomSheetRef.current?.expand()}
+              >
+                <Heading color="white">{storagedName || 'crew member'}</Heading>
+                <Box
+                  borderStyle="dotted"
+                  borderColor="accent"
+                  borderWidth={1}
+                />
+              </TouchableOpacity>
+            </Box>
+          </Box>
+        </AnimatedBox>
+
         <NextLaunch />
-      </AnimatedBox>
 
-      <AnimatedBox delay={650}>
         <UpcomingLaunches />
-      </AnimatedBox>
 
-      <AnimatedBox delay={800}>
         <PastLaunches />
-      </AnimatedBox>
-    </ScrollView>
+      </ScrollView>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        keyboardBehavior="interactive"
+        keyboardBlurBehavior="restore"
+        backdropComponent={renderBackdrop}
+        backgroundStyle={{
+          backgroundColor: theme.colors.background,
+        }}
+        handleComponent={() => (
+          <Box w="100%" alignItems="center" justifyContent="center">
+            <Box bg="primary" mt={4} rounded={8} w={10} h={1.5} />
+          </Box>
+        )}
+        enablePanDownToClose
+        enableOverDrag
+      >
+        <Box flex={1} mt="12" px="4">
+          <Heading color="white">what you would like to be called?</Heading>
+
+          <Box alignItems="center">
+            <BottomSheetTextInput
+              autoCapitalize="words"
+              autoCompleteType="name"
+              value={name}
+              onChangeText={setName}
+              style={{
+                borderBottomColor: 'white',
+                borderBottomWidth: 1.6,
+                marginTop: 24,
+                marginBottom: 36,
+                width: '100%',
+                height: 48,
+                color: 'white',
+                fontSize: 22,
+                fontWeight: '500',
+              }}
+            />
+
+            <TouchableOpacity
+              onPress={handleSubmitName}
+              disabled={!name}
+              style={{
+                backgroundColor: theme.colors.secondary,
+                padding: 10,
+                width: '100%',
+                alignItems: 'center',
+                borderRadius: 16,
+              }}
+            >
+              <Text color="white" fontSize="sm" fontWeight={700}>
+                Confirm
+              </Text>
+            </TouchableOpacity>
+          </Box>
+        </Box>
+      </BottomSheet>
+    </>
   );
 }
 
