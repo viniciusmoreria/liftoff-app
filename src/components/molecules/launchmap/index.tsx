e7889cb7b47335b8d1e419f26abef9f94792a243
Vinícius Moreira <viniciusmoreeira@icloud.com>
2022-01-22 16:37:33 -0300

    feat: implements map zoom toggle feature (#32)

--- src/components/molecules/launchmap/index.tsx
+++ src/components/molecules/launchmap/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Ionicons } from '@expo/vector-icons';
 import { Linking, Platform } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
@@ -11,6 +12,8 @@ import { customMapStyle } from './customStyle';
 
 function LaunchMap({ launchpad }: { launchpad: Launchpad }) {
   const { latitude, longitude, locality, region, name } = launchpad;
+
+  const [isZoomApplied, toggleZoom] = React.useReducer((s) => !s, false);
 
   const handleOpenGps = React.useCallback(() => {
     const scheme = Platform.select({
@@ -28,47 +31,107 @@ function LaunchMap({ launchpad }: { launchpad: Launchpad }) {
   }, [latitude, longitude, name]);
 
   return (
-    <Atoms.Card sx={{ p: 0 }}>
-      <MapView
-        scrollEnabled={false}
-        zoomEnabled={false}
-        pitchEnabled={false}
-        rotateEnabled={false}
-        region={{
-          latitude: launchpad.latitude,
-          longitude: launchpad.longitude,
-          latitudeDelta: 0.1,
-          longitudeDelta: 20,
-        }}
-        style={{ height: 200, width: '100%', borderRadius: 8 }}
-        provider={PROVIDER_GOOGLE}
-        customMapStyle={customMapStyle}
-        onPress={() => {
-          handleOpenGps();
-        }}
-      >
-        <Marker
-          icon={CaretDownIcon}
-          coordinate={{
-            latitude: launchpad.latitude,
-            longitude: launchpad.longitude,
+    <Atoms.Pressable
+      onPress={() => {
+        handleOpenGps();
+      }}
+    >
+      <Atoms.Card sx={{ p: 0 }}>
+        <MapView
+          scrollEnabled={false}
+          zoomEnabled={false}
+          pitchEnabled={false}
+          rotateEnabled={false}
+          region={{
+            latitude,
+            longitude,
+            latitudeDelta: isZoomApplied ? 0.1 : 13,
+            longitudeDelta: 0.01,
           }}
-        />
-      </MapView>
-
-      <Atoms.Card
-        sx={{
-          position: 'absolute',
-          bottom: 0,
-          borderTopLeftRadius: 0,
-          borderTopRightRadius: 0,
-          bg: '#252525',
-        }}
-      >
-        <Atoms.Row
-          sx={{ alignItems: 'center', justifyContent: 'space-between' }}
+          style={{ height: 200, width: '100%', borderRadius: 8 }}
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={customMapStyle}
         >
-          <Atoms.Box>
+          <Marker
+            coordinate={{
+              latitude,
+              longitude,
+            }}
+            opacity={0.8}
+          >
+            <Atoms.Box>
+              <Atoms.Image
+                source={CaretDownIcon}
+                resizeMode="contain"
+                sx={{
+                  height: 30,
+                }}
+              />
+            </Atoms.Box>
+          </Marker>
+        </MapView>
+
+        <Atoms.Pressable
+          sx={{
+            zIndex: 1,
+            position: 'absolute',
+            right: 0,
+          }}
+          onPress={toggleZoom}
+        >
+          <Atoms.Card
+            sx={{
+              width: 30,
+              height: 30,
+              mt: '12px',
+              alignItems: 'center',
+              justifyContent: 'center',
+              p: 0,
+              bg: '#252525',
+            }}
+          >
+            <Ionicons
+              name={isZoomApplied ? 'remove' : 'add'}
+              color="white"
+              size={18}
+            />
+          </Atoms.Card>
+        </Atoms.Pressable>
+
+        <Atoms.Card
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0,
+            bg: '#252525',
+          }}
+        >
+          <Atoms.Row
+            sx={{ alignItems: 'center', justifyContent: 'space-between' }}
+          >
+            <Atoms.Box>
+              <Atoms.Text
+                variant="text-xs"
+                sx={{
+                  color: 'white',
+                  fontWeight: 'bold',
+                }}
+              >
+                {locality}
+              </Atoms.Text>
+
+              <Atoms.Text
+                variant="text-xs"
+                sx={{
+                  color: 'primary',
+                  mt: '10px',
+                }}
+              >
+                {region}
+              </Atoms.Text>
+            </Atoms.Box>
+
             <Atoms.Text
               variant="text-xs"
               sx={{
@@ -76,32 +139,12 @@ function LaunchMap({ launchpad }: { launchpad: Launchpad }) {
                 fontWeight: 'bold',
               }}
             >
-              {locality}
+              {name}
             </Atoms.Text>
-
-            <Atoms.Text
-              variant="text-xs"
-              sx={{
-                color: 'primary',
-                mt: '10px',
-              }}
-            >
-              {region}
-            </Atoms.Text>
-          </Atoms.Box>
-
-          <Atoms.Text
-            variant="text-xs"
-            sx={{
-              color: 'white',
-              fontWeight: 'bold',
-            }}
-          >
-            {name}
-          </Atoms.Text>
-        </Atoms.Row>
+          </Atoms.Row>
+        </Atoms.Card>
       </Atoms.Card>
-    </Atoms.Card>
+    </Atoms.Pressable>
   );
 }
 
