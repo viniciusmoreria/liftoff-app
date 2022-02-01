1179fce476ca4b281c2f06fb66b83ae74a881d09
Vinícius Moreira <viniciusmoreeira@icloud.com>
2022-02-01 12:26:13 -0300

    style: new versioning section style

--- .versionrc.js
+++ .versionrc.js
@@ -16,4 +16,17 @@ module.exports = {
       updater: require.resolve('standard-version-expo/ios'),
     },
   ],
+  types: [
+    { type: 'feat', section: '✨ Features' },
+    { type: 'fix', section: '🐛 Bug Fixes' },
+    { type: 'perf', section: '🚀 Performance Improvements' },
+    { type: 'refactor', section: '🛠 Refactors' },
+    { type: 'revert', section: '🔙 Reverts' },
+    { type: 'docs', section: '📚 Documentation' },
+    { type: 'style', section: '💄 Styles' },
+    { type: 'test', section: '📦 Tests' },
+    { type: 'chore', section: '🤖 Chores' },
+    { type: 'build', section: '🛠 Build System', hidden: true },
+    { type: 'ci', hidden: true },
+  ],
 };
