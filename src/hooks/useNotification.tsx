6b734d0080ff44db2818de43836cce05d5c6854b
Vinícius Moreira <viniciusmoreeira@icloud.com>
2022-02-02 16:33:17 -0300

    fix: improve notifications messages (#49)
    * fix: improve notifications messages
    
    * feat: new sheduled notification at t-minus 60 minutes

--- src/hooks/useNotification.tsx
+++ src/hooks/useNotification.tsx
@@ -34,10 +34,33 @@ const NotificationProvider: React.FC = ({ children }) => {
 
     await Notifications.scheduleNotificationAsync({
       content: {
-        title: `${launch.name}: T-Minus 10 minutes`,
-        body: `Flight #${launch?.flight_number} launching from ${launch.launchpad.locality}`,
+        title: 'Upcoming Launch',
+        body: `${launch.name} will attempt launch from ${launch.launchpad.locality} - ${launch.launchpad.region} in 24 hours`,
       },
       trigger: {
+        // 24 hours before launch
+        seconds: launchTime - 24 * 60 * 60,
+      },
+    });
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Upcoming Launch',
+        body: `${launch.name} will attempt launch from ${launch.launchpad.locality} - ${launch.launchpad.region} in 1 hour`,
+      },
+      trigger: {
+        // 1 hour before launch
+        seconds: launchTime - 60 * 60,
+      },
+    });
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: `${launch.name}: T-Minus 10 minutes`,
+        body: `Flight #${launch?.flight_number} ready for launch at ${launch.launchpad.locality}`,
+      },
+      trigger: {
+        // 10 minutes before launch
         seconds: launchTime - 600,
       },
     });
@@ -45,9 +68,10 @@ const NotificationProvider: React.FC = ({ children }) => {
     await Notifications.scheduleNotificationAsync({
       content: {
         title: `${launch.name} aboard ${launch.rocket.name}`,
-        body: `Commencing in T-Minus 5 minutes`,
+        body: `Liftoff in T-Minus 5 minutes`,
       },
       trigger: {
+        // 5 minutes before launch
         seconds: launchTime - 300,
       },
     });
