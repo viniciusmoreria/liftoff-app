206e481252a4f9540deafa1bf06938fd83968102
Vinícius Moreira <viniciusmoreeira@icloud.com>
2022-02-02 12:43:03 -0300

    feat: implements sentry to catch errors in production (#47)

--- app.config.js
+++ app.config.js
@@ -15,5 +15,17 @@ module.exports = function ({ config }) {
         },
       },
     },
+    hooks: {
+      postPublish: [
+        {
+          file: 'sentry-expo/upload-sourcemaps',
+          config: {
+            organization: process.env.SENTRY_ORG,
+            project: process.env.SENTRY_PROJECT,
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+          },
+        },
+      ],
+    },
   };
 };
--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { DripsyProvider } from 'dripsy';
 import * as Updates from 'expo-updates';
-import { AppStateStatus, Platform, StatusBar } from 'react-native';
+import { AppStateStatus, LogBox, Platform, StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { focusManager, QueryClient, QueryClientProvider } from 'react-query';
+import * as Sentry from 'sentry-expo';
 
 import {
   BottomSheetProvider,
@@ -16,15 +17,33 @@ import {
 import { Routes } from './routes';
 import { theme } from './styles/theme';
 
+Sentry.init({
+  dsn: 'https://da82bac74a644e358c63c3377d820c05@o1133485.ingest.sentry.io/6180063',
+  environment: Updates.releaseChannel,
+});
+
+if (__DEV__) {
+  /*
+    react-query causes these types of logs to be shown but they say that they are nothing to worry about
+    this may be able to be removed after upgrading to RN 0.65
+    ref: https://github.com/tannerlinsley/react-query/issues/1259
+  */
+  LogBox.ignoreLogs(['Setting a timer']);
+}
+
+const oneMinute = 1000 * 60;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 2, staleTime: oneMinute },
+  },
+});
+
 function onAppStateChange(status: AppStateStatus) {
   if (Platform.OS !== 'web') {
     focusManager.setFocused(status === 'active');
   }
 }
-
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { retry: 2, cacheTime: 60000 } },
-});
 
 export default function App() {
   useOnlineManager();
