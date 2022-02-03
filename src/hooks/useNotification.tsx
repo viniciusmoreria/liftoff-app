bfe4fa7503f7e71e5127aca44c2fa5f934338de8
Vinícius Moreira <viniciusmoreeira@icloud.com>
2022-02-03 10:54:29 -0300

    fix: notification scheduling calculation (#50)

--- src/hooks/useNotification.tsx
+++ src/hooks/useNotification.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext } from 'react';
 
 import * as Notifications from 'expo-notifications';
+import * as Sentry from 'sentry-expo';
 
 import type { LaunchProps } from '@types';
 
@@ -30,51 +31,58 @@ const NotificationProvider: React.FC = ({ children }) => {
       return;
     }
 
-    const launchTime = new Date(launch.date_local).getTime() / 1000;
+    try {
+      const secondsToLaunch = Math.round(
+        new Date(launch.date_local).getTime() / 1000 -
+          new Date().getTime() / 1000,
+      );
 
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: 'Upcoming Launch',
-        body: `${launch.name} will attempt launch from ${launch.launchpad.locality} - ${launch.launchpad.region} in 24 hours`,
-      },
-      trigger: {
-        // 24 hours before launch
-        seconds: launchTime - 24 * 60 * 60,
-      },
-    });
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Upcoming Launch',
+          body: `${launch.name} will attempt launch from ${launch.launchpad.locality} - ${launch.launchpad.region} in 24 hours`,
+        },
+        trigger: {
+          // 24 hours before launch
+          seconds: secondsToLaunch - 86400,
+        },
+      });
 
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: 'Upcoming Launch',
-        body: `${launch.name} will attempt launch from ${launch.launchpad.locality} - ${launch.launchpad.region} in 1 hour`,
-      },
-      trigger: {
-        // 1 hour before launch
-        seconds: launchTime - 60 * 60,
-      },
-    });
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Upcoming Launch',
+          body: `${launch.name} will attempt launch from ${launch.launchpad.locality} - ${launch.launchpad.region} in 1 hour`,
+        },
+        trigger: {
+          // 1 hour before launch
+          seconds: secondsToLaunch - 60 * 60,
+        },
+      });
 
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: `${launch.name}: T-Minus 10 minutes`,
-        body: `Flight #${launch?.flight_number} ready for launch at ${launch.launchpad.locality}`,
-      },
-      trigger: {
-        // 10 minutes before launch
-        seconds: launchTime - 600,
-      },
-    });
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: `${launch.name}: T-Minus 10 minutes`,
+          body: `Flight #${launch?.flight_number} ready for launch at ${launch.launchpad.locality}`,
+        },
+        trigger: {
+          // 10 minutes before launch
+          seconds: secondsToLaunch - 600,
+        },
+      });
 
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: `${launch.name} aboard ${launch.rocket.name}`,
-        body: `Liftoff in T-Minus 5 minutes`,
-      },
-      trigger: {
-        // 5 minutes before launch
-        seconds: launchTime - 300,
-      },
-    });
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: `${launch.name} aboard ${launch.rocket.name}`,
+          body: `Liftoff in T-Minus 5 minutes. Livestream is now available`,
+        },
+        trigger: {
+          // 5 minutes before launch
+          seconds: secondsToLaunch - 300,
+        },
+      });
+    } catch (err) {
+      Sentry.Native.captureException(err);
+    }
   }
 
   return (
