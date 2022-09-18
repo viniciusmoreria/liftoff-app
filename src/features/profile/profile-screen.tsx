e7ce29d3ded14e43000f262763a8061a5d3bf8be
Vinícius Moreira <viniciusmoreeira@icloud.com>
2022-09-18 16:10:21 -0300

    feat: include app version

--- src/features/profile/profile-screen.tsx
+++ src/features/profile/profile-screen.tsx
@@ -3,6 +3,7 @@ import { Image, Text, View } from 'react-native';
 import { PlaceholderUserPicture } from '@assets/images';
 import { Container } from '@components/container';
 import { useUserStore } from '@store/userStore';
+import * as Application from 'expo-application';
 
 import { NotificationSection } from './components/notification-section';
 import { SupportSection } from './components/support-section';
@@ -13,17 +14,23 @@ export const ProfileScreen = () => {
   return (
     <Container useScrollView>
       <View className="flex-1 px-8">
-        <View className="items-center">
-          <Image className="h-20 w-20 rounded-full" source={PlaceholderUserPicture} />
-          <View className="flex-1 justify-items-center mt-4">
-            <Text numberOfLines={2} className="text-xl text-white font-semibold">
-              {username ?? 'crew member'}
-            </Text>
+        <View className="flex-1">
+          <View className="items-center">
+            <Image className="h-20 w-20 rounded-full" source={PlaceholderUserPicture} />
+            <View className="flex-1 justify-items-center mt-4">
+              <Text numberOfLines={2} className="text-xl text-white font-semibold">
+                {username ?? 'crew member'}
+              </Text>
+            </View>
           </View>
+          <View className="border-[1px] border-secondary my-8 rounded" />
+          <NotificationSection />
+          <SupportSection />
         </View>
-        <View className="border-[1px] border-secondary my-8 rounded" />
-        <NotificationSection />
-        <SupportSection />
+
+        <Text className="text-xs text-white font-medium text-center">
+          Version {Application.nativeApplicationVersion}
+        </Text>
       </View>
     </Container>
   );
