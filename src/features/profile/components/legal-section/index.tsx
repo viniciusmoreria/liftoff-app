41a99c6346127228300299f52ee4d05a02f23e07
Vinícius Moreira <viniciusmoreeira@icloud.com>
2022-09-23 18:33:50 -0300

    feat: insert profile section functions

--- src/features/profile/components/legal-section/index.tsx
+++ src/features/profile/components/legal-section/index.tsx
@@ -1,17 +1,21 @@
-import { Text, View } from 'react-native';
+import { Linking, Text, View } from 'react-native';
 
 import { MenuItem } from '../menu-item';
 
 export const LegalSection = () => {
+  async function openPrivacyPolicy() {
+    await Linking.openURL('https://liftoffprivacypolicy.carrd.co/');
+  }
+
   return (
     <View className="mt-6">
       <Text className="text-lg text-white font-bold">Legal</Text>
       <View className="justify-items-center space-y-6 bg-secondary p-4 rounded-lg mt-4">
-        <View>
+        {/* <View>
           <MenuItem title="Acknowledgments" onPress={() => null} />
-        </View>
+        </View> */}
         <View>
-          <MenuItem title="Privacy Policy" onPress={() => null} />
+          <MenuItem title="Privacy Policy" onPress={openPrivacyPolicy} />
         </View>
       </View>
     </View>
--- src/features/profile/components/support-section/index.tsx
+++ src/features/profile/components/support-section/index.tsx
@@ -1,23 +1,57 @@
-import { Text, View } from 'react-native';
+import { Linking, Share, Text, View } from 'react-native';
+
+import { isAndroid } from '@libs/utilities';
+import * as Application from 'expo-application';
+import * as Device from 'expo-device';
 
 import { MenuItem } from '../menu-item';
 
 export const SupportSection = () => {
+  async function sendEmail({ title }: { title: string }) {
+    const reportInfo = {
+      Version: Application.nativeApplicationVersion,
+      Device: `${Device.manufacturer} ${Device.modelName}`,
+      OS: `${Device.osName} ${Device.osVersion}`,
+      Timezone: new Date().toTimeString().slice(9, 17),
+    };
+    const deviceData = Object.entries(reportInfo)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join('\n');
+
+    await Linking.openURL(
+      `mailto:appliftoff@gmail.com?subject=Liftoff ${title} Request (v${Application.nativeApplicationVersion})&body=\n\n\n\n${deviceData}`
+    );
+  }
+
+  async function rateApp() {
+    if (isAndroid) {
+      await Linking.openURL(`market://details?id=${Application.applicationId}`);
+    } else {
+      await Linking.openURL(`itms-apps://itunes.apple.com/app/id=1645685152`);
+    }
+  }
+
+  async function shareApp() {
+    await Share.share({
+      message: `Track and watch all upcoming rocket launches from agencies around the world with Liftoff.\n\nDownload Liftoff for iOS: https://apps.apple.com/us/app/liftoff/id1645685152 \n\nDownload Liftoff for Android: https://play.google.com/store/apps/details?id=${Application.applicationId}`,
+    });
+  }
+
   return (
     <View className="mt-6">
       <Text className="text-lg text-white font-bold">Support</Text>
       <View className="justify-items-center space-y-6 bg-secondary p-4 rounded-lg mt-4">
         <View>
-          <MenuItem title="Bug Report" onPress={() => null} />
+          <MenuItem title="Bug Report" onPress={() => sendEmail({ title: 'Support' })} />
         </View>
         <View>
-          <MenuItem title="Feature Request" onPress={() => null} />
+          <MenuItem title="Feature Request" onPress={() => sendEmail({ title: 'Feature' })} />
         </View>
         <View>
-          <MenuItem title="Rate Our App" onPress={() => null} />
+          <MenuItem title="Rate Our App" onPress={rateApp} />
         </View>
         <View>
-          <MenuItem title="Share" onPress={() => null} />
+          <MenuItem title="Share" onPress={shareApp} />
         </View>
       </View>
     </View>
