8681c4168d4c508cf0f3f48065cebb03954f0298
Vinícius Moreira <viniciusmoreeira@icloud.com>
2022-10-05 20:06:29 -0300

    feat: new countdown animation

--- src/features/home/components/countdown/index.tsx
+++ src/features/home/components/countdown/index.tsx
@@ -1,11 +1,10 @@
 import { Pressable, Text, View } from 'react-native';
 
 import { Launch } from '@features/home/hooks/types';
-import { UPCOMING_LAUNCHES_QUERY_KEY } from '@features/home/hooks/use-upcoming-launches';
+import { useUpcomingLaunches } from '@features/home/hooks/use-upcoming-launches';
 import { getLaunchStage } from '@libs/utilities';
-import { useQueryClient } from '@tanstack/react-query';
 import { isAfter } from 'date-fns';
-import Animated, { FadeIn } from 'react-native-reanimated';
+import Animated, { FadeIn, FadeInDown, FadeOutUp } from 'react-native-reanimated';
 
 import { useCountdown } from './hooks/use-coundown';
 
@@ -14,8 +13,7 @@ type Props = {
 };
 
 export const Countdown = ({ navigateToLaunchDetail }: Props) => {
-  const queryClient = useQueryClient();
-  const data = queryClient.getQueryData<Launch[]>([UPCOMING_LAUNCHES_QUERY_KEY]);
+  const { data } = useUpcomingLaunches();
 
   const nextLaunch = data?.find((launch) =>
     isAfter(
@@ -38,8 +36,8 @@ export const Countdown = ({ navigateToLaunchDetail }: Props) => {
   const shouldShowSeconds = !shouldShowDays && !shouldShowHours;
 
   return (
-    <Pressable onPress={() => navigateToLaunchDetail(nextLaunch)}>
-      <Animated.View entering={FadeIn} className="flex-row mt-8 px-4">
+    <Animated.View entering={FadeIn}>
+      <Pressable onPress={() => navigateToLaunchDetail(nextLaunch)} className="flex-row mt-8 px-4">
         <View className="flex-1 justify-around mr-6">
           <Text className="text-sm text-white font-bold" numberOfLines={2}>
             {nextLaunch?.mission?.name ?? nextLaunch?.name}
@@ -52,7 +50,8 @@ export const Countdown = ({ navigateToLaunchDetail }: Props) => {
 
         {shouldShowDays && (
           <View>
-            <View className="items-center bg-secondary px-2 py-4 rounded-md min-w-[50]">
+            <View className="items-center bg-secondary px-2 py-5 rounded-md min-w-[50]">
+              <NumberDisplay displayNumber={days} />
               <Text className="text-white text-2xl font-bold">{days}</Text>
             </View>
             <Text className="text-gray text-xs mt-1 pb-1">{Number(days) > 1 ? 'days' : 'day'}</Text>
@@ -75,8 +74,8 @@ export const Countdown = ({ navigateToLaunchDetail }: Props) => {
         {shouldShowSeconds && (
           <CountdownDigit digit={seconds} label={Number(seconds) > 1 ? 'secs' : 'sec'} />
         )}
-      </Animated.View>
-    </Pressable>
+      </Pressable>
+    </Animated.View>
   );
 };
 
@@ -92,8 +91,8 @@ const CountdownDigit = ({ digit, label, showSeparator = true }: CountdownDigitPr
       {showSeparator && <Text className="text-white text-2xl mx-1 mt-3">:</Text>}
       <View>
         <View className="flex-row items-center">
-          <View className="items-center bg-secondary px-2 py-4 rounded-md min-w-[50]">
-            <Text className="text-white text-2xl font-bold">{digit}</Text>
+          <View className="items-center bg-secondary px-2 py-5 rounded-md min-w-[50] overflow-hidden">
+            <NumberDisplay displayNumber={digit} />
           </View>
         </View>
         <Text className="text-gray text-xs mt-1 pb-1">{label}</Text>
@@ -101,3 +100,20 @@ const CountdownDigit = ({ digit, label, showSeparator = true }: CountdownDigitPr
     </View>
   );
 };
+
+function NumberDisplay({ displayNumber }: { displayNumber: string }) {
+  return (
+    <View className="flex-row items-center justify-center">
+      {displayNumber.split('').map((digit, index) => (
+        <Animated.Text
+          key={`${index}-${digit}`}
+          entering={FadeInDown}
+          exiting={FadeOutUp}
+          className="text-white text-2xl font-bold min-w-[15] text-center"
+        >
+          {digit}
+        </Animated.Text>
+      ))}
+    </View>
+  );
+}
